210e094f833120d0ee44cd0819220e5851475dd4
Mayank16092003 <mayankaa81@gmail.com>
2025-01-23 10:58:18 +0530

    commit level 1

---
+++ src/Day2/Level1/ConversionInMilesAndYards.java
@@ -0,0 +1,13 @@
+package Day2.Level1;
+import java.util.Scanner;
+public class ConversionInMilesAndYards {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of feet you would like to convert");
+        Scanner input= new Scanner(System.in);
+        int feet= input.nextInt();
+        float yards=feet/3f;
+        float mile=yards/1760;
+        System.out.println("distance in yards is "+yards+" distancce in miles is "+mile);
+
+    }
+}
---
+++ src/Day2/Level1/ConversionOfKmToMiles.java
@@ -0,0 +1,9 @@
+package Day2.Level1;
+
+public class ConversionOfKmToMiles {
+    public static void main(String[] args) {
+        float distanceInKm = 10.8f;
+        float distanceInMiles = distanceInKm / 1.6f;
+        System.out.println("The distance " + distanceInKm + " in Miles is " + distanceInMiles);
+    }
+}
